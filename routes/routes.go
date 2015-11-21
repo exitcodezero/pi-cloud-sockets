455da0df754a0abc19163fce53dfe28bd1a7de2f
Brian Hines <brian@projectweekend.net>
2015-11-20 22:39:45 -0600

    Make socket info route optionally enabled

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/exitcodezero/picloud/config"
 	"github.com/exitcodezero/picloud/info"
 	"github.com/exitcodezero/picloud/middleware"
 	"github.com/exitcodezero/picloud/socket"
@@ -12,18 +13,23 @@ import (
 // Router setups all the API routes and middleware
 func Router() *mux.Router {
 	common := alice.New(middleware.Authentication, middleware.RecoverHandler)
+	noAuth := alice.New(middleware.RecoverHandler)
 
-	infoHandlers := handlers.MethodHandler{
-		"GET": common.ThenFunc(info.Handler),
+	infoSocket := handlers.MethodHandler{
+		"GET": noAuth.ThenFunc(info.SocketHandler),
 	}
 
-	socketHandlers := handlers.MethodHandler{
+	pubSubSocket := handlers.MethodHandler{
 		"GET": common.ThenFunc(socket.Handler),
 	}
 
 	router := mux.NewRouter()
-	router.Handle("/connect", socketHandlers)
-	router.Handle("/info", infoHandlers)
+
+	router.Handle("/connect", pubSubSocket)
+
+	if config.EnableInfoSocket != "" {
+		router.Handle("socket/info", infoSocket)
+	}
 
 	return router
 }
