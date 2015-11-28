dfa46d178dfd903d90341e26c0b359a27c04f522
Brian Hines <brian@projectweekend.net>
2015-11-28 17:08:43 -0600

    gofmt

--- publish/handlers.go
+++ publish/handlers.go
@@ -1,40 +1,40 @@
 package publish
 
 import (
-    "encoding/json"
+	"encoding/json"
 	"github.com/exitcodezero/picloud/hub"
-    "github.com/exitcodezero/picloud/message"
+	"github.com/exitcodezero/picloud/message"
 	"net/http"
 	"time"
 )
 
 type publishBody struct {
-    Event     string    `json:"event"`
-	Data      string    `json:"data"`
+	Event string `json:"event"`
+	Data  string `json:"data"`
 }
 
 // Handler publishes messages via HTTP
 func Handler(w http.ResponseWriter, r *http.Request) {
-    decoder := json.NewDecoder(r.Body)
-    pb := publishBody{}
-    err := decoder.Decode(&pb)
-    if err != nil {
-        panic(err)
-    }
+	decoder := json.NewDecoder(r.Body)
+	pb := publishBody{}
+	err := decoder.Decode(&pb)
+	if err != nil {
+		panic(err)
+	}
 
-    if pb.Event == "" {
+	if pb.Event == "" {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 
-    m := message.SocketMessage{
-        Action: "publish",
-        Event: pb.Event,
-        Data: pb.Data,
-        CreatedAt: time.Now().UTC(),
-    }
+	m := message.SocketMessage{
+		Action:    "publish",
+		Event:     pb.Event,
+		Data:      pb.Data,
+		CreatedAt: time.Now().UTC(),
+	}
 
-    hub.Manager.Publish(m)
+	hub.Manager.Publish(m)
 
-    w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusCreated)
 }
